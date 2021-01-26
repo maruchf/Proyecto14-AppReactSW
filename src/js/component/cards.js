e393948c6b908f8565e86cfae5a3c9276bbdee4d
MARIA CHACON <E10140@TMVE.local>
2021-01-26 12:25:39 -0400

    add changes with async await function and useEffect for People of SW

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,53 +1,25 @@
 import React from "react";
+import Proptypes from "prop-types";
 import { Link } from "react-router-dom";
 // Estos cards deben ser creados con un bucle..pendiente
-export const Cards = () => {
+export const Cards = ({ item }) => {
 	return (
-		<div className="row row-cols-1 row-cols-md-3 g-4">
-			<div className="col">
-				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							This is a wider card with supporting text below as a natural lead-in to additional content.
-							This content is a little bit longer.
-						</p>
-					</div>
-					<div className="card-footer">
-						<small className="text-muted">Last updated 3 mins ago</small>
-					</div>
-				</div>
-			</div>
-			<div className="col">
-				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							This card has supporting text below as a natural lead-in to additional content.
-						</p>
-					</div>
-					<div className="card-footer">
-						<small className="text-muted">Last updated 3 mins ago</small>
-					</div>
-				</div>
-			</div>
-			<div className="col">
-				<div className="card h-100">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							This is a wider card with supporting text below as a natural lead-in to additional content.
-							This card has even longer content than the first to show that equal height action.
-						</p>
-					</div>
-					<div className="card-footer">
-						<small className="text-muted">Last updated 3 mins ago</small>
-					</div>
-				</div>
+		<div className="card m-2">
+			<img className="card-img-top" src="..." alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{item.name}</h5>
+				<p className="card-text">
+					This is a wider card with supporting text below as a natural lead-in to additional content. This
+					content is a little bit longer.
+				</p>
+				<p className="card-text">
+					<small className="text-muted">Last updated 3 mins ago</small>
+				</p>
 			</div>
 		</div>
 	);
+};
+
+Cards.propTypes = {
+	item: Proptypes.object
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,21 +1,51 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 import "../../styles/home.scss";
 import { Navbar } from "../component/navbar";
 import { Cards } from "../component/cards";
 import { Link } from "react-router-dom";
-export const Home = () => (
-	<div className="container-fluid bg-dark">
-		<div className="row">
-			<h1 className="text-white">Browser Databank //</h1>
-		</div>
-		<div className="row">
-			<div className="col-3">
-				<Navbar />
+
+export const Home = props => {
+	const BASE_URL = "https://swapi.dev/api"; //url de la doc
+	const [people, setPeople] = useState([]); //useState para personajes-peopleSW
+	useEffect(() => {
+		const getPeople = async url => {
+			try {
+				let response = await fetch(url);
+				let responseObject = await response.json();
+				setPeople(responseObject.results);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		let url = `${BASE_URL}/people`;
+		getPeople(url);
+	}, []);
+	//hacer fetch o async await a la api para obtener personajes
+	//Endpoint a usar People
+	// tiene sentido usar async await para no hacer fetch dentro de una promesa
+	//.the... incluso puede hacerse un ciclo el fetch no es async
+	// fetch(url)
+	// 	.then(response => response.json()) //convierte respuesta a json
+	// 	.then(responseObject => setPeople(responseObject.results)) // con los personajes enviar estado
+	//.catch(error => console.log(error)); // captura errores
+	return (
+		<div className="container-fluid bg-dark pt-4">
+			<div className="row border-secondary border-top border-bottom ">
+				<h3 className="text-white pl-3 pt-1">BROWSER DATABANK //</h3>
 			</div>
-			<div className="col-9">
-				<Cards />
+			<div className="row pt-4">
+				<div className="col-3">
+					<Navbar />
+				</div>
+				<div className="card-holder col-9 d-flex justify-content-start">
+					{people.map((people, index) => {
+						//envío información como propiedad item (desde objeto people) y el
+						//index debido a que en la respuesta de la API no vi un id
+						return <Cards key={index} item={people} />;
+					})}
+					;
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
